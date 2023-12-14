fd835fe9adea47ba1d2459087ead24292de6d4b8
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-12-14 13:28:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "6293739020"))
+API_HASH = getenv("API_HASH", "ee6df88753c36eeab95391940ba3844f")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv("BOT_TOKEN", "6293739020:AAEWNsNNG5pzU5K1RQ2by7xPdT9ua0nA5YY")
+OWNER_ID = int(getenv("OWNER_ID", "6088155585"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://karjr002:INCyT5eXfqjoiYQ4@cluster0.6swcped.mongodb.net/?retryWrites=true&w=majority")
